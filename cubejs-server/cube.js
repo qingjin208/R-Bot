40fb179de543c5cfa537b4b3d4ffcc627c3b7f7f
Neil Zhen <QingJin208@163.com>
2026-08-24 10:09:52 +0800

    fix: remove invalid jwtSecret, keep apiSecret only

--- cubejs-server/cube.js
+++ cubejs-server/cube.js
@@ -24,7 +24,6 @@ const server = new CubejsServer({
     });
   },
   schemaPath: 'schema',
-  jwtSecret: process.env.CUBEJS_JWT_SECRET || 'your-secret-key',
   apiSecret: process.env.CUBEJS_JWT_SECRET || 'your-secret-key',
 });
 
